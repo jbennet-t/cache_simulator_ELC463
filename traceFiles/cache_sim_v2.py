c8f2f7b063778dc200d7594c3324c032fcb148db
jbennet-t <sinowayjb@gmail.com>
2021-02-20 17:17:25 -0500

    progress ?

--- traceFiles/cache_sim_v2.py
+++ traceFiles/cache_sim_v2.py
@@ -5,6 +5,16 @@
 
 import math
 
+def set_counter(index_loc, N, cache):
+    set_cntr = int(cache[index_loc][N+1])
+
+    if(set_cntr == N):
+        set_cntr = 1 #set to 1 bc location 0 in the list is the "index" val
+    else:
+        set_cntr += 1
+    cache[index_loc][N+1] = set_cntr #where the set_cntr val is stored
+
+
 def main():
     data = []
     KN = 64
@@ -35,16 +45,11 @@ def main():
         i += 3
 
 
-    #get ready to hold the data as a list of objects, keep track of the offsets
-    #dataSets = [] 
-    #indexes = []
-
     #lower 3 bits are cleared so, ignore bits 21-23
 
     index_size = int(math.log2(K)) # 1,2,3,4
     index_start = 21 - index_size
     #index = entry[index_start:21] #21 is stop bit, not included
-    #print(index)
 
     # offset = entry[:]
     # offsets.append(offset)
@@ -52,33 +57,8 @@ def main():
     #as stated in handout
 
     #tag = entry[0:index_start] #setting tag
-    # dataSet = Sets(tag, index)
-    # dataSets.append(dataSet)
-    #validBit = entry[0]
-    
-    #print(tag)
-
-
-    #creating the cache
-    # cache = []
-    # i = 0
-    # while i < N:
-    #     cache.append(Sets([""]*K),"")
-    #     i += 1
-    #     # print(cache[i-1].tag)
-    #     # print(dataSets[i-1].tag)
-
-    #     #[ idx  00 01 10 11  cntr
-    #     # set 1['a','b','c','d', x]
-    #     # set 2['','','','', x]
-    #     # set 3['','','','', x]
-    #     # set 4['','','','', x]
-    #     #]
-
-    #     # 01_a 00_b 11_a 01_a 00_c
 
     #creating cache 2, electric boogaloo
-
     cache = []
     idx = 2**index_size
 
@@ -91,34 +71,71 @@ def main():
 
    # print('[%s]' % ', '.join(map(str, cache)))
 
-    print(cache[0][0])
-
+     #print(cache[0][0])
         
 
+    # #-------------------------------------------------------------
+    # #old LRU
+    # #-------------------------------------------------------------
+    # for entry in reorderedData:
+    #     for cacheItem in cache:
+    #         for i in range(N):
+    #             set_cntr = 0 #variable for counting spot in set
+    #             if cacheItem[i][0] == entry[index_start:21]: #if the index of the trace data == the index of the cache
+    #                 if cacheItem[i][set_cntr] == 0: #if the space is empty
+    #                     cacheItem[i][set_cntr] = entry[0:index_start] #fill empty space with tag
+    #                     MISS += 1
+    #                     set_cntr += 1 #increment spot in set since its filled
+    #                     cacheItem[i][N-2] = set_cntr #update set counter
+    #                 elif cacheItem[i][set_cntr] != 0 and cacheItem[i][set_cntr] == entry[0:index_start]: #if not empty & the same
+    #                     HIT += 1
+    #                 else: # cacheItem.tag[0] != "" and cacheItem.tag != entry.tag:
+    #                     cacheItem[i][set_cntr] = entry[0:index_start]
+    #                     MISS += 1
+    #                     set_cntr += 1 #increment spot in set since its filled
+    #                     cacheItem[i][N-2] = set_cntr #update set counter
+    #             # else: #entry.index != cacheItem.index
+    #             #     MISS += 1
+    #             # break
+    # #--------------------------------------------------------------
+
+    empty = 0
+    hit = 0
+    overwrite = 0
     #-------------------------------------------------------------
-    #LRU
+    #new LRU
     #-------------------------------------------------------------
+    set_cntr = 1 #variable for counting spot in set
+    i = 0
     for entry in reorderedData:
-        for cacheItem in cache:
-            for i in range(N):
-                set_cntr = 0 #variable for counting spot in set
-                if cacheItem[i][0] == entry[index_start:21]: #if the index of the trace data == the index of the cache
-                    if cacheItem[i][set_cntr] == 0: #if the space is empty
-                        cacheItem[i][set_cntr] = entry[0:index_start] #fill empty space with tag
-                        MISS += 1
-                        set_cntr += 1 #increment spot in set since its filled
-                        cacheItem[i][17] = set_cntr #update set counter
-                    elif cacheItem[i][set_cntr] != 0 and cacheItem[i][set_cntr] == entry[0:index_start]: #if not empty & the same
-                        HIT += 1
-                    else: # cacheItem.tag[0] != "" and cacheItem.tag != entry.tag:
-                        cacheItem[i][set_cntr] = entry[0:index_start]
-                        MISS += 1
-                        set_cntr += 1 #increment spot in set since its filled
-                        cacheItem[i][17] = set_cntr #update set counter
-                # else: #entry.index != cacheItem.index
-                #     MISS += 1
-                # break
+        for i in range(K):
+            if cache[i][0] == entry[index_start:21]: #if the index of the trace data == the index of the cache
+                if cache[i][set_cntr] == 0: #if the space is empty
+                    cache[i][set_cntr] = entry[0:index_start] #fill empty space with tag
+                    MISS += 1
+                    set_counter(i, N, cache) #increment and check set counter
+                    empty += 1
+                    print(i)
+                elif cache[i][set_cntr] != 0 and cache[i][set_cntr] == entry[0:index_start]: #if not empty & the same
+                    HIT += 1
+                    #set_counter(i, N, cache) #increment and check set counter
+                    hit += 1
+                else: # cacheItem.tag[0] != "" and cacheItem.tag != entry.tag:
+                    cache[i][set_cntr] = entry[0:index_start]
+                    MISS += 1
+                    set_counter(i, N, cache)
+                    overwrite += 1
+
+            # else: #entry.index != cacheItem.index
+            #     MISS += 1
+            # break
     #--------------------------------------------------------------
+
+    print('[%s]' % ', '.join(map(str, cache)))
+    print("empty:", empty)
+    print("hit:", hit)
+    print("overwrite:", overwrite)
+
     #-------------------------------------------------------------- 
 #     #FIFO
 #     #--------------------------------------------------------------
